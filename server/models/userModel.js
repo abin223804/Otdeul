668b2da13a34f22b3d7a8967043442a2ec54f944
Abin s chandran <abinschandran@gmail.com>
2024-05-23 12:03:18 +0530

    userSchema created

---
+++ server/models/userModel.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: "string", required: true },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePic: {
+      type: String,
+    },
+
+    role: { type: String },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", function (next) {
+  this.updatedAt = Date.now();
+  next();
+});
+
+const userModel = mongoose.model("user", userSchema);
+
+module.exports = userModel;
