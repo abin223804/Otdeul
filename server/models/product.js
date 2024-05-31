28e417e4f34f9698b9cdb797038e414a5ebf1b58
Abin s chandran <abinschandran@gmail.com>
2024-05-31 17:30:28 +0530

    images changed to array

--- server/models/product.js
+++ server/models/product.js
@@ -17,18 +17,24 @@ const reviewSchema = mongoose.Schema(
 
 const productSchema = new Schema(
     {
-      name: { type: String, required: true },
-      image: { type: String, required: true },
-      quantity: { type: Number, required: true },
+      productName: { type: String, required: true },
+      images: [{ type: String, required: true }], 
       category: { type: ObjectId, ref: "Category" }, // Optional category reference
       subcategory: { type: ObjectId, ref: "Subcategory" }, // Optional subcategory reference
       description: { type: String, required: true },
       reviews: [reviewSchema],
       rating: { type: Number, required: true, default: 0 },
       numReviews: { type: Number, required: true, default: 0 },
+      dummyPrice: { type: Number, required: true, default: 0 },
+      offerPercentage: { type: Number, required: true,
       price: { type: Number, required: true, default: 0 },
-      countInStock: { type: Number, required: true, default: 0 },
+      Stock: { type: Number, required: true, default: 0 },
+      color: { type: String, required: true },
+      size: { type: String, required: true },
+      refund: { type: Boolean, default:true},
+
     },
+  },
     { timestamps: true }
   );
   
