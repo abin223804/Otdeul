64954a81ab762126a24885f38e0652407167819f
Abin s chandran <abinschandran@gmail.com>
2024-06-01 16:35:17 +0530

    usermodel updated with review schema

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -1,6 +1,12 @@
-import Product from '../models/product.js'; // Assuming the file structure
-import upload from '../middlewares/multerMiddleware.js';
-import asyncHandler from '../middlewares/asyncHandler.js';
+import Product from "../models/product.js"; // Assuming the file structure
+import upload from "../middlewares/multerMiddleware.js";
+import asyncHandler from "../middlewares/asyncHandler.js";
+
+// for admin 👇
+
+
+
+// Create product
 
 
 const createProduct = asyncHandler(async (req, res) => {
@@ -10,7 +16,9 @@ const createProduct = asyncHandler(async (req, res) => {
         return res.status(400).json({ message: err.message });
       }
 
-      const basePath = `${req.protocol}://${req.get("host")}/public/uploads/product`;
+      const basePath = `${req.protocol}://${req.get(
+        "host"
+      )}/public/uploads/product`;
       const productVariations = [];
 
       const variations = req.body.variations; // assuming variations is already parsed correctly
@@ -19,23 +27,27 @@ const createProduct = asyncHandler(async (req, res) => {
         const variationData = variations[varIndex];
         const sizes = [];
 
-        for (let sizeIndex = 0; sizeIndex < variationData.sizes.length; sizeIndex++) {
+        for (
+          let sizeIndex = 0;
+          sizeIndex < variationData.sizes.length;
+          sizeIndex++
+        ) {
           const sizeData = variationData.sizes[sizeIndex];
           const fieldName = `variations[${varIndex}][sizes][${sizeIndex}][images]`;
-          const images = (req.files || []).filter(file =>
-            file.fieldname === fieldName
-          ).map(file => basePath + '/' + file.filename);
+          const images = (req.files || [])
+            .filter((file) => file.fieldname === fieldName)
+            .map((file) => basePath + "/" + file.filename);
 
           sizes.push({
             size: sizeData.size,
             stock: parseInt(sizeData.stock, 10),
-            images: images
+            images: images,
           });
         }
 
         productVariations.push({
           color: variationData.color,
-          sizes: sizes
+          sizes: sizes,
         });
       }
 
@@ -47,18 +59,49 @@ const createProduct = asyncHandler(async (req, res) => {
         description: req.body.description,
         rating: req.body.rating || 0,
         numReviews: req.body.numReviews || 0,
-        refund: req.body.refund || true
+        refund: req.body.refund || true,
       });
 
       await newProduct.save();
       res.status(201).json(newProduct);
     });
   } catch (error) {
-    res.status(500).json({ message: 'Server error', error });
+    res.status(500).json({ message: "Server error", error });
   }
 });
 
 
+// Read or view product(all)
+
+// Read or view product(by id/category)
+
+//update product
+
+//Delete product
+
+//manage customer review (delete/reply)
+
+
+
+
+
+
+// for user 👇
+
+
+//product browse by category
+
+//product detail view
+
+//product variant selection(size,color)
+
+//product add to cart and wishlist
+
+// product review and rating
+
+//product comparison
+
+//product recommendations
 
 
 
@@ -70,223 +113,8 @@ const createProduct = asyncHandler(async (req, res) => {
 
 
 
-
-
-
-
-
-
-
-// Controller function to create a new product
-// const createProduct = async (req, res) => {
-//   try {
-//     const { name, image, quantity, category, subcategory, description, price, countInStock } = req.body;
-//     const product = await Product.create({
-//       name,
-//       image,
-//       quantity,
-//       category,
-//       subcategory,
-//       description,
-//       price,
-//       countInStock,
-//     });
-//     res.status(201).json(product);
-//   } catch (error) {
-//     res.status(400).json({ message: error.message });
-//   }
-// };
-
-// // Controller function to get all products
-// const getProducts = async (req, res) => {
-//   try {
-//     const products = await Product.find().populate('category subcategory');
-//     res.json(products);
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// };
-
-// // Controller function to get a single product by ID
-// const getProductById = async (req, res) => {
-//   try {
-//     const product = await Product.findById(req.params.id).populate('category subcategory');
-//     if (!product) {
-//       return res.status(404).json({ message: 'Product not found' });
-//     }
-//     res.json(product);
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// };
-
-// // Controller function to update a product by ID
-// const updateProduct = async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const { name, image, quantity, category, subcategory, description, price, countInStock } = req.body;
-//     const product = await Product.findByIdAndUpdate(
-//       id,
-//       { name, image, quantity, category, subcategory, description, price, countInStock },
-//       { new: true }
-//     ).populate('category subcategory');
-//     if (!product) {
-//       return res.status(404).json({ message: 'Product not found' });
-//     }
-//     res.json(product);
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// };
-
-// // Controller function to delete a product by ID
-// const deleteProduct = async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const product = await Product.findByIdAndDelete(id);
-//     if (!product) {
-//       return res.status(404).json({ message: 'Product not found' });
-//     }
-//     res.json({ message: 'Product deleted successfully' });
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// };
-
-// // Controller function to get all reviews of a product
-// const getProductReviews = async (req, res) => {
-//   try {
-//     const { productId } = req.params;
-//     const product = await Product.findById(productId);
-    
-//     if (!product) {
-//       return res.status(404).json({ message: 'Product not found' });
-//     }
-    
-//     const reviews = product.reviews;
-    
-//     res.json(reviews);
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// };
-
-// // Controller function to create a review for a product
-// const createReview = async (req, res) => {
-//   try {
-//     const { productId } = req.params;
-//     const { name, rating, comment } = req.body;
-//     const product = await Product.findById(productId);
-
-//     if (!product) {
-//       return res.status(404).json({ message: 'Product not found' });
-//     }
-
-//     const review = {
-//       name,
-//       rating,
-//       comment,
-//       user: req.user._id, // Assuming you have authentication middleware that sets req.user
-//     };
-
-//     product.reviews.push(review);
-//     product.numReviews += 1;
-//     product.rating =
-//       (product.rating * (product.numReviews - 1) + rating) / product.numReviews;
-
-//     await product.save();
-
-//     res.status(201).json({ message: 'Review added successfully', review });
-//   } catch (error) {
-//     res.status(400).json({ message: error.message });
-//   }
-// };
-
-// // Controller function to update a review of a product
-// const updateReview = async (req, res) => {
-//   try {
-//     const { productId, reviewId } = req.params;
-//     const { rating, comment } = req.body;
-//     const product = await Product.findById(productId);
-
-//     if (!product) {
-//       return res.status(404).json({ message: 'Product not found' });
-//     }
-
-//     const review = product.reviews.find((r) => r._id.toString() === reviewId);
-
-//     if (!review) {
-//       return res.status(404).json({ message: 'Review not found' });
-//     }
-
-//     // Check if the user is authorized to update the review (optional)
-//     if (review.user.toString() !== req.user._id.toString()) {
-//       return res.status(401).json({ message: 'Not authorized to update this review' });
-//     }
-
-//     // Update the review fields
-//     review.rating = rating;
-//     review.comment = comment;
-
-//     // Recalculate the product rating
-//     product.rating =
-//       (product.reviews.reduce((acc, item) => item.rating + acc, 0)) /
-//       product.reviews.length;
-
-//     await product.save();
-
-//     res.json({ message: 'Review updated successfully', review });
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// };
-
-// // Controller function to delete a review of a product
-// const deleteReview = async (req, res) => {
-//   try {
-//     const { productId, reviewId } = req.params;
-//     const product = await Product.findById(productId);
-//     if (!product) {
-//       return res.status(404).json({ message: 'Product not found' });
-//     }
-
-//     const review = product.reviews.find((r) => r._id.toString() === reviewId);
-
-//     if (!review) {
-//       return res.status(404).json({ message: 'Review not found' });
-//     }
-
-//     // Check if the user is authorized to delete the review (optional)
-//     if (review.user.toString() !== req.user._id.toString()) {
-//       return res.status(401).json({ message: 'Not authorized to delete this review' });
-//     }
-
-//     product.reviews.pull(review);
-//     product.numReviews -= 1;
-
-//     if (product.numReviews === 0) {
-//       product.rating = 0;
-//     } else {
-//       product.rating =
-//         (product.rating * (product.numReviews + 1) - review.rating) / product.numReviews;
-//     }
-
-//     await product.save();
-
-//     res.json({ message: 'Review deleted successfully' });
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// };
 
 export default {
   createProduct,
-  // getProducts,
-  // getProductById,
-  // updateProduct,
-  // deleteProduct,
-  // getProductReviews,
-  // createReview,
-  // updateReview,
-  // deleteReview,
+
 };
--- server/models/product.js
+++ server/models/product.js
@@ -2,14 +2,20 @@ import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
 
+const reviewSchema = new Schema({
+  review: { type: String, required: false },
+  rating: { type: Number, required: false },
+  user: { type: Schema.Types.ObjectId, ref: 'User' } // Assuming 'User' is the referenced model
+});
+
 const sizeSchema = new Schema({
-  size: { type: String, required: true },
+  size: { type: String, required: true }, 
   stock: { type: Number, required: true },
-  images: [{ type: String, required: true }]
+  images: [{ type: String, required: true }] 
 });
 
 const variationSchema = new Schema({
-  color: { type: String, required: true },
+  color: { type: String, required: false },
   sizes: [sizeSchema]
 });
 
@@ -21,7 +27,7 @@ const productSchema = new Schema(
     subcategory: { type: Schema.Types.ObjectId, ref: 'Subcategory', required: true },
     description: { type: String, required: true },
     rating: { type: Number, default: 0 },
-    numReviews: { type: Number, default: 0 },
+    reviews: [reviewSchema], 
     refund: { type: Boolean, default: true }
   },
   { timestamps: true }
