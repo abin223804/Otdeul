0cd0ae838cb211a50d8bf5fd6bd75932153e9421
Abin s chandran <abinschandran@gmail.com>
2024-06-21 11:30:21 +0530

    brandschema generated

---
+++ server/models/brand.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const BrandSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+  },
+  image: {
+    data: Buffer,
+    contentType: String,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+
+  updated: Date,
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+
+const Brand = mongoose.model("Brand", BrandSchema);
+
+export default Brand;
