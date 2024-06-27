0a5b9e8a7f51513b815bdfb59f3a9bdc5052b6de
Abin s chandran <abinschandran@gmail.com>
2024-06-27 10:33:58 +0530

    ةعمفثق ؤخىبهلعثس سثحشقشفثي

---
+++ server/config/multerConfig.js
@@ -0,0 +1,34 @@
+import multer from 'multer';
+import path from 'path';
+
+const storage = multer.diskStorage({
+  destination: './uploads/',
+  filename: function(req, file, cb){
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
+
+function checkFileType(file, cb){
+  const filetypes = /jpeg|jpg|png|gif/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+
+  if(mimetype && extname){
+    return cb(null, true);
+  } else {
+    cb('Error: Images Only!');
+  }
+}
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 5000000 }, 
+  fileFilter: function(req, file, cb){
+    checkFileType(file, cb);
+  }
+}).fields([
+  { name: 'thumbnails', maxCount: 3 },
+  { name: 'variations[].photo', maxCount: 10 } 
+]);
+
+export default upload;
